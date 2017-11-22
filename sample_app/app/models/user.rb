f72dfd1fdf48b2abe65180a1af8e21799b01f3d7
adiblaze4 <aditya.tomar100@gmail.com>
2017-11-22 11:31:44 +0530

    New version Sample App

---
+++ sample_app/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  #“Foo@ExAMPle.CoM” and “foo@example.com” same so covert the email to downcase and then save to database
+  before_save { self.email = email.downcase} # callback
+
+  validates :name,  presence: true, length: { minimum: 5 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
